3585e16ef1018aaa4d3abbfab62529104efaa96e
nathanlee543 <nathanlee543@gmail.com>
2022-09-03 17:09:49 +0200

    added columns to output

--- simulations/two_nested_subclones/two_nested_subclones_simulation.c
+++ simulations/two_nested_subclones/two_nested_subclones_simulation.c
@@ -310,7 +310,7 @@ double expansion(double b[3], double d[3], double t1, double t2, double t, doubl
             #pragma omp critical (write_estimates)
             {
                 fp = fopen("nested_subclones_simulation_results.txt","a");    
-                fprintf(fp, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",r_hat, r1_hat, r2_hat, u_hat, t1_hat, t2prime_hat, t_hat, m1, m2, m1_obs, m2_obs, subclonal, alpha1c, alpha2c, beta1c, beta2c);
+                fprintf(fp, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",r_hat, r1_hat, r2_hat, u_hat, t1_hat, t2prime_hat, t_hat, m1, m2, m1_obs, m2_obs, subclonal, alpha1c, alpha2c, beta1c, beta2c,M1,M2,cells[0],cells[1],cells[2],WBC1[0],WBC1[1],WBC1[2]);
                 fclose(fp);
             }
             // free memory
@@ -443,7 +443,7 @@ int main(int argc, char *argv[]){
         
         // add a header to the output file
         fp = fopen("nested_subclones_simulation_results.txt","w");    
-        fprintf(fp,"r_hat,r1_hat,r2_hat,u_hat,t1_hat,t2prime_hat,t_hat,m1,m2,m1_obs,m2_obs,gamma,alpha1c,alpha2c,beta1c,beta2c\n");
+        fprintf(fp,"r_hat,r1_hat,r2_hat,u_hat,t1_hat,t2prime_hat,t_hat,m1,m2,m1_obs,m2_obs,gamma,alpha1c,alpha2c,beta1c,beta2c,M1,M2,cells[0],cells[1],cells[2],WBC1[0],WBC1[1],WBC1[2]\n");
         fclose(fp);
 
         // input file determines if we multithread
